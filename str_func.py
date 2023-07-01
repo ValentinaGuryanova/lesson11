42f8cb47177916839c47710c101f1ccb237ed947
valentina <guryanovavv21@gmail.com>
2023-07-02 02:31:28 +0300

    new str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def big_letters(messages):
+    """принимает на вход строку и возвращает ее со всеми заглавными буквами"""
+
     return messages.upper()
+
+def first_big_letter(messages):
+    """делает заглавными первые буквы каждого слова в строке"""
+
+    return messages.title()
+
